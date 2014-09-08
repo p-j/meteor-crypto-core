96b2950945c67335def89ff1ac7609acb7b8a80e
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 18:05:42 +0200

    fix git url

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	summary: 'Base package for CryptoJS, standard secure crypto algorithms',
 	version: '3.1.2-1',
-	git: 'https://github.com/p-j/meteor-crypto-base.git'
+	git: 'https://github.com/p-j/meteor-crypto-base'
 });
 
 Package.on_use(function (api) {
