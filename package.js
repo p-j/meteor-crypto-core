56a5fddce993def50c00edb526a900cb925e12ed
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 17:34:12 +0200

    Update for Meteor 0.9.x new package system

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
-  summary: "Base package for CryptoJS, standard secure crypto algorithms"
+	summary: 'Base package for CryptoJS, standard secure crypto algorithms',
+	version: '3.1.2-1',
+	git: 'https://github.com/p-j/meteor-crypto-base.git'
 });
 
 Package.on_use(function (api) {
-  api.add_files('crypto.js', ['client', 'server']);
-  api.export && api.export('CryptoJS');
+	api.versionsFrom('METEOR@0.9.0');
+	api.add_files('crypto.js', ['client', 'server']);
+	if (api.export) {
+		api.export('CryptoJS');
+	}
 });
